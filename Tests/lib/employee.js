b01670731f0273c3ea8e33aa6ec28a59a52867db
Joe Small <small.joseph@outlook.com>
2021-04-07 00:11:51 -0400

    added methods to employee class - exported file

--- Tests/lib/employee.js
+++ Tests/lib/employee.js
@@ -6,5 +6,19 @@ class Employee   {
     this.officeNumber = officeNumber;
 }
 
+getEmployeeName() {
+    return `The employee's name is ${this.name}`
+}
+getEmployeeId() {
+    return `The employee's id is ${this.name}`
+}
+getEmailAddress() {
+        return `The employee's email address is ${this.emailAddress}`
+}
+getOfficeNumber() {
+        return `The employee's office number is ${this.officeNumber}`
+}
 
 }
+
+module.exports = Employee;
