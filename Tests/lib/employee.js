cd64a1e76fd201515cd2caad6a346af366a6a441
Joe Small <small.joseph@outlook.com>
2021-04-07 00:08:39 -0400

    employee class edit

---
+++ Tests/lib/employee.js
@@ -0,0 +1,10 @@
+class Employee   {
+    constructor(name, emplouyeeId, emailAddress, officeNumber) {
+    this.name = name;
+    this.employeeId = employeeId;
+    this.emailAddress = emailAddress;
+    this.officeNumber = officeNumber;
+}
+
+
+}
