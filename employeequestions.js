445fdcd9a8751f4d497836f50dfa283a50c6ef80
Joe Small <small.joseph@outlook.com>
2021-04-09 09:41:16 -0400

    render html

--- employeequestions.js
+++ employeequestions.js
@@ -2,16 +2,25 @@ module.exports = [
     {
         type: 'input',
         name: 'name',
-        message: 'Please enter the name of the employee',
+        message: 'Enter the name of the employee',
     },
     {
-        type: 'input',
-        name: 'title',
-        message: 'Please enter the id of the employee',
+        message: 'Enter the employee id number of the employee',
+        name: 'id'
     },
     {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter the email address of the employee',
-    }
+        message: 'Enter the email address of the employeee',
+        name: 'email'
+    },
+    {
+        type: 'list',
+        message: 'Enter the role of the employee',
+        choices: [
+            'Intern',
+            'Engineer',
+            'Manager'
+        ],
+        name: "role"
+    },
+
 ];
--- index.js
+++ index.js
@@ -1,85 +1,39 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const internquestions = require('./internquestions');
-const employeequestions = require('./employeequestions');
-const engineerquestions = require('./engineerquestions');
-const managerquestions = require('./managerquestions');
+const employee= require('./lib/employee');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const manager = require('./lib/manager');
+const employeeQuestions = require('./employeequestions')
 
 
-const generateHTML = (answers) => {
-    ` <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1> The employee's name is ${employee.name}</h1>
-    <h1> The employee's id ${employee.id}</h1>
-    <h1> The employee's email is ${employee.email}</h1>
+const employeeList = [];
 
 
-    <h1> The managers's name is ${manager.name}</h1>
-    <h1> The manager's id ${manager.id}</h1>
-    <h1> The manager's email is ${manager.email}</h1>
+const initEmployee = () => {
+inquirer.prompt(employeeQuestions).then(
 
-    <h1> The engineer's name is ${engineer.name}</h1>
-    <h1> The engineer's id ${engineer.id}</h1>
-    <h1> The engineer's email is ${engineer.email}</h1>
+).then(function({name, role, id, email}) {
+    let questionByRole = "";
+    if (role === 'Intern') {
+        questionByRole = 'School'
+    }else if (role === 'Engineer') {
+        questionByRole = 'github username'
+    }else {
+        questionByRole = 'office number'
+    }
+inquirer.prompt([{
+message: `Enter the ${questionByRole} of the employee`,
+name: 'questionByRole'
+}])
 
-     <h1> The intern's name is ${intern.name}</h1>
-    <h1> The intern's id ${intern.id}</h1>
-    <h1> The intern's email is ${intern.email}</h1>
 
-    <script src="index.js"></script>
-</body>
-</html>`
+
+})
 }
 
-// promopt a list of what employee to be
-// .then ({type})
-// if engineer createEngineer()
+const renderHTML = () => {
 
-// function createTeam (){
-// inquirer.prompt
-// [
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: 'Please enter the name of the employee',
-//     },
-//     {
-//         type: 'input',
-//         name: 'title',
-//         message: 'Please enter the id of the employee',
-//     },
-//     {
-//         type: 'input',
-//         name: 'description',
-//         message: 'Please enter the email address of the employee',
-//     }
-// ]
-// ).then ([]) => {
-//     if (role = engineer) {
-
-//     }
-
-// if else statement
+}
 
 
-
-
-inquirer
-    .prompt(internquestions)
-    .prompt(engineerquestions)
-    .prompt(managerquestions)
-    .prompt(employeequestions)
-    .then((data) => {
-        const filename = `index.html`;
-        fs.writeFile(filename, generateHTML(data), (err) =>
-            err ? console.log(err) : console.log('Success!')
-        );
-    });
