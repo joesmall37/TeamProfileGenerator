badfc803323195ea657f5ba7b05c0c07730426c4
Joe Small <small.joseph@outlook.com>
2021-04-09 07:54:13 -0400

    setting up prompts

--- index.js
+++ index.js
@@ -39,6 +39,39 @@ const generateHTML = (answers) => {
 </html>`
 }
 
+// promopt a list of what employee to be
+// .then ({type})
+// if engineer createEngineer()
+
+// function createTeam (){
+// inquirer.prompt
+// [
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: 'Please enter the name of the employee',
+//     },
+//     {
+//         type: 'input',
+//         name: 'title',
+//         message: 'Please enter the id of the employee',
+//     },
+//     {
+//         type: 'input',
+//         name: 'description',
+//         message: 'Please enter the email address of the employee',
+//     }
+// ]
+// ).then ([]) => {
+//     if (role = engineer) {
+
+//     }
+
+// if else statement
+
+
+
+
 inquirer
     .prompt(internquestions)
     .prompt(engineerquestions)
